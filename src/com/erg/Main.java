4622adcb89616ebe57fbd7f81ac1e25479c51f64
haci47 <ergulhaci339@gmail.com>
2021-02-20 17:56:34 +0200

    Initial commit

---
+++ src/com/erg/Main.java
@@ -0,0 +1,17 @@
+package com.erg;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a1 = "Elma";
+        String a2 = "Armut";
+        String a3 = "Ceviz";
+        boolean a1contains = a1.contains("a");
+        boolean a2contains = a2.contains("a");
+        boolean a3contains = a3.contains("a");
+        System.out.println("a1contains :" +a1contains);
+        System.out.println("a2contains :" +a2contains);
+        System.out.println("a3contains :" +a3contains);
+
+    }
+}
---
+++ src/string2.java
@@ -0,0 +1,11 @@
+public class string2 {
+    public static void main(String[] args) {
+        String a1 = "Hello";
+        String a2 = "hello";
+        char ındex_char = a1.charAt(4);
+        System.out.println(" ındex_char :" + ındex_char);
+        System.out.println(a1.equals(a2));
+        System.out.println(a1.equalsIgnoreCase(a2));
+
+    }
+}
